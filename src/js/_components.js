2d5fbdbf9ad24e460943c2885a487b59e4e90155
Maksden567 <babadid346@gmail.com>
2022-10-16 22:18:21 +0300

    title-block

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,1 +1,11 @@
-console.log('components');
+ const burgerBtn=document.querySelector('.burger__btn')
+ const closeBtn=document.querySelector('.close__btn')
+ const right=document.querySelector('.header__right')
+
+
+burgerBtn.addEventListener('click',function(){
+   right.style.top="0px"
+})
+closeBtn.addEventListener('click',function(){
+    right.style.top="-100%"
+ })
